eee879533f1a837cc851c43bf7b4230da998175a
Abdullah Younus <abdullah1001k@gmail.com>
2023-06-24 20:23:23 -0700

    navbar done

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,9 +1,35 @@
+import Link from 'next/link';
 import logo from '/public/logo.webp';
 import Image from 'next/image';
 const Header = () => {
     return (
-        <div>
+        <div className='flex justify-between items-center py-6 px-8'>
             <Image src={logo} alt='logo' />
+            <ul className='flex gap-x-10'>
+                <li className='text-lg'>
+                    <Link href={"#"}>
+                        Female
+                    </Link>
+                </li>
+                <li className='text-lg'>
+                    <Link href={"#"}>
+                        Male
+                    </Link>
+                </li>
+                <li className='text-lg'>
+                    <Link href={"#"}>
+                        Kids
+                    </Link>
+                </li>
+                <li className='text-lg'>
+                    <Link href={"#"}>
+                        All Products
+                    </Link>
+                </li>
+            </ul>
+            <div className='h-10 w-10 rounded-full bg-gray-200'>
+
+            </div>
         </div>
     )
 }

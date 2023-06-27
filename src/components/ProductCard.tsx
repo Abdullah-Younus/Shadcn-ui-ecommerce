af85c316534c4dac1917165af3e7d812b0058d7f
Abdullah-Younus <abdullah1001k@gmail.com>
2023-06-28 01:38:04 +0500

    local changes

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,18 +1,19 @@
-import Image from "next/image";
+import Image, { StaticImageData } from 'next/image';
 import p1 from '/public/p1.png';
 import { FC } from "react";
 
 interface ProductData {
     title: string,
-    price: string
+    price: number,
+    img: StaticImageData
 }
 
-export const ProductCard = ({ title, price }: ProductData) => {
+export const ProductCard = ({ title, price, img }: ProductData) => {
     return (
         <div>
             <Image src={p1} alt='Picture' />
             <h3 className='font-bold text-lg mt-3'>{title}</h3>
-            <p className='font-bold text-lg'>{price}</p>
+            <p className='font-bold text-lg'>${price}</p>
         </div>
     )
 }
--- src/views/ProductList.tsx
+++ src/views/ProductList.tsx
@@ -1,5 +1,5 @@
 import p1 from '/public/p1.png';
-import Image from 'next/image';
+import Image, { StaticImageData } from 'next/image';
 import { ProductCard } from '@/components/ProductCard';
 
 const ProductList = () => {
@@ -7,9 +7,9 @@ const ProductList = () => {
 
     return (
         <div className='flex justify-evenly mt-16'>
-            <ProductCard title="Flex Sweater" price="$16" />
-            <ProductCard title="Jeans Flex" price="$16" />
-            <ProductCard title="Kurti Sweater" price="$16" />
+            <ProductCard title="Flex Sweater" price={10} img={p1}/>
+            <ProductCard title="Jeans Flex" price={50} img={p1}/>
+            <ProductCard title="Kurti Sweater" price={16} img={p1}/>
 
 
         </div>

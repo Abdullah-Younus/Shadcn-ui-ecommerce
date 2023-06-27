bc0d383c96172adbacaa85492eb02c4114407d66
Abdullah-Younus <abdullah1001k@gmail.com>
2023-06-28 01:26:33 +0500

    local changs

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,12 +1,18 @@
 import Image from "next/image";
 import p1 from '/public/p1.png';
+import { FC } from "react";
 
-export const ProductCard = () => {
+interface ProductData {
+    title: string,
+    price: string
+}
+
+export const ProductCard = ({ title, price }: ProductData) => {
     return (
         <div>
             <Image src={p1} alt='Picture' />
-            <h3 className='font-bold text-lg mt-3'>Flex Sweater</h3>
-            <p className='font-bold text-lg'>$104</p>
+            <h3 className='font-bold text-lg mt-3'>{title}</h3>
+            <p className='font-bold text-lg'>{price}</p>
         </div>
     )
 }
--- src/views/ProductList.tsx
+++ src/views/ProductList.tsx
@@ -1,10 +1,17 @@
 import p1 from '/public/p1.png';
 import Image from 'next/image';
 import { ProductCard } from '@/components/ProductCard';
+
 const ProductList = () => {
+
+
     return (
-        <div>
-            <ProductCard />
+        <div className='flex justify-evenly mt-16'>
+            <ProductCard title="Flex Sweater" price="$16" />
+            <ProductCard title="Jeans Flex" price="$16" />
+            <ProductCard title="Kurti Sweater" price="$16" />
+
+
         </div>
     )
 }

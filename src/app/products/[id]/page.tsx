1b9d79099d010e91f36b624d3cf294ff0136e63b
Abdullah-Youns <126175476+Abdullah-Youns@users.noreply.github.com>
2023-06-30 15:42:20 +0500

    local changes

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -4,7 +4,7 @@ import Image, { StaticImageData } from "next/image";
 
 
 const getProductDetails = (id: number) => {
-    return products.filter((product) => product.id === id);
+    return products.filter((product) => product.id == id);
 }
 
 

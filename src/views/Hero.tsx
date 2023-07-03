acce84dd6a37c316da5223009271a7a77c69cc5b
Abdullah-Younus <abdullah1001k@gmail.com>
2023-07-03 23:16:01 +0500

    local changes

--- src/views/Hero.tsx
+++ src/views/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
     return (
         <section className="flex flex-col lg:flex-row gap-y-10 py-6">
             {/* left side  */}
-            <div className="flex-1">
+            <div className="flex-1 ">
                 <Badge className="py-3 px-6 rounded-lg bg-blue-200 text-blue-700 hover:bg-none">
                     Sale 70%
                 </Badge>
@@ -18,11 +18,20 @@ const Hero = () => {
                     spent all day lounging on his throne. One day, his advisors came to him
                     with a problem: the kingdom was running out of money.
                 </p>
-                <Button className="bg-black h-12 px-8 mt-4">Start Shopping</Button>
+                <Button className="bg-black h-12 px-8 mt-4">
+                    Start Shopping
+                </Button>
+                <div>
+                    {/* <Image src={"/public/featured1.jpg"} alt="Featured One" width={200} height={200} />
+                    <Image src={"/public/featured2.jpg"} alt="Featured Two" width={150} height={150} />
+                    <Image src={"/public/featured3.jpg"} alt="Featured Three" width={150} height={150} />
+                    <Image src={"/public/featured4.jpg"} alt="Featured Four" width={150} height={150} /> */}
+                </div>
+
             </div>
             {/* Right side  */}
-            <div className="flex-1 bg-[#ffece3] rounded-full w-auto h-auto">
-                <Image src={heroImage} alt="Hero Image"/>
+            <div className="hidden md:flex  flex-1 bg-[#ffece3] rounded-full w-auto h-auto">
+                <Image src={heroImage} alt="Hero Image" />
             </div>
         </section>
     )

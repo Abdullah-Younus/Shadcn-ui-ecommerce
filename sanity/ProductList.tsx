e19090bf3d576668232806178be0783e66f8aeb0
Abdullah-Younus <abdullah1001k@gmail.com>
2023-07-04 17:43:38 +0500

    local changes

---
+++ sanity/ProductList.tsx
@@ -0,0 +1,80 @@
+import { defineField } from "sanity";
+
+export const product = {
+    name: "product",
+    type: "document",
+    title: "Product",
+    fields: [
+        {
+            name: "title",
+            title: "Product Title",
+            type: "string",
+        },
+        {
+            name: "slug",
+            title: "Slug",
+            type: "slug",
+            options: {
+                source: "title",
+                maxLength: 200,
+                slugify: (input: string) => input
+                    .toLowerCase()
+                    .replace(/\s+/g, '-')
+                    .slice(0,200)
+            }
+        },
+        {
+            name: "description",
+            title: "Product Description",
+            type: "array",
+            of: [
+                {
+                    type: "block"
+                }
+            ]
+        },
+        {
+            name: "price",
+            title: "Product Price",
+            type: "number",
+        },
+        {
+            name: "image",
+            title: "Product Image",
+            type: "array",
+            of: [
+                {
+                    type: "image",
+                    fields: [
+                        {
+                            name: "alt",
+                            type: "text",
+                            title: "Alternative text"
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            name: "productTypes",
+            type: "array",
+            title: "Product Type",
+            of: [
+                {
+                    type: "string"
+                }
+            ]
+        },
+
+        // defineField({
+        //     name: "category",
+        //     title: "Product Category",  
+        //     to: [
+        //         {
+        //           type: "category",
+        //         },
+        //         /// other document type
+        //       ],
+        // }),
+    ],
+};
--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,5 +1,6 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import { product } from './ProductList'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [product],
 }

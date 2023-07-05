fa462d5781e2a8d9ee399d0d633812a58f16486f
Abdullah-Younus <abdullah1001k@gmail.com>
2023-07-06 00:58:33 +0500

    local changes

--- src/lib/sanityClient.ts
+++ src/lib/sanityClient.ts
@@ -1,9 +1,9 @@
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-    apiVersion:'2023-07-03',
-    dataset:'production',
-    projectId:process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-    useCdn:true
-    // token:process.env.NEXT_PUBLIC_SANITY_ACCESS_TOKEN,
+    apiVersion: '2023-07-03',
+    dataset: 'production',
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    // token: process.env.NEXT_PUBLIC_SANITY_ACCESS_TOKEN,
+    useCdn: true
 }) 

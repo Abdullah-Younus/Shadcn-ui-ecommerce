fa45d7e1ab25aa664d80b51ac44bae5d8d6f09e1
Abdullah-Younus <abdullah1001k@gmail.com>
2023-07-05 15:27:59 +0500

    local changes

--- src/views/ProductTypes/index.tsx
+++ src/views/ProductTypes/index.tsx
@@ -13,7 +13,7 @@ const ProductType = () => {
                 </h3>
             </div>
             <div className='grid grid-cols-4 gap-6 px-2 text-gray-800'>
-                <div className='w-full flex h-auto col-span-2 px-12 bg-[#d6d6d8]'>
+                <div className='w-full flex h-auto col-span-2 rounded-md px-12 bg-[#d6d6d8]'>
                     <div className='max-w-[13rem] py-8'>
                         <h4 className="scroll-m-20 text-3xl font-extrabold tracking-tight">
                             GET UP TO 60%
@@ -26,7 +26,7 @@ const ProductType = () => {
                         <Image src={"https://full-stack-ecommerce-clothing-web.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fevent1.6f776995.png&w=384&q=75"} alt="product 1" width={1000} height={1000} />
                     </div>
                 </div>
-                <div className='w-full row-span-2 h-full bg-[#efe1c7]'>
+                <div className='w-full row-span-2 h-full rounded-md  bg-[#efe1c7]'>
                     <div className='p-4'>
                         <p>
                             Flex Sweatshirts
@@ -39,7 +39,7 @@ const ProductType = () => {
                         <Image src={"https://full-stack-ecommerce-clothing-web.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fevent2.b5f201ac.png&w=384&q=75"} alt='Banner Image' width={1000} height={1000} />
                     </div>
                 </div>
-                <div className='w-full row-span-2 h-full bg-[#d7d7d9]'>
+                <div className='w-full row-span-2 h-full rounded-md  bg-[#d7d7d9]'>
                     <div className='p-4'>
                         <p>
                             Flex Push Button Bomber
@@ -52,14 +52,14 @@ const ProductType = () => {
                         <Image src={"https://full-stack-ecommerce-clothing-web.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fevent3.798fa92b.png&w=384&q=75"} alt='Banner Image' width={1000} height={1000} />
                     </div>
                 </div>
-                <div className='text-white w-full col-span-2 bg-[#212121] flex flex-col justify-center items-center space-y-4'>
+                <div className='text-white w-full rounded-md  col-span-2 bg-[#212121] flex flex-col justify-center items-center space-y-4'>
                     <h3 className='text-white font-semibold text-4xl'>
                         GET 30% Off
                     </h3>
                     <p>
                         USE PROMO CODE
                     </p>
-                    <button className='py-1 px-6 bg-[#474747] w-72 rounded-lg tracking-[5px]'> DINEWEEKENDSALE </button>
+                    <button className='py-1 px-6 text-lg bg-[#474747] w-72 rounded-md  tracking-[5px]'> DINEWEEKENDSALE </button>
                 </div>
             </div>
         </div>

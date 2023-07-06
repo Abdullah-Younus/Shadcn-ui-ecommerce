a115a66efb8728ee7a99a4d0156b3a7ab5b1b427
Abdullah-Younus <abdullah1001k@gmail.com>
2023-07-06 13:48:51 +0500

    local changes

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: true,
   images: {
-    domains: ['full-stack-ecommerce-clothing-web.vercel.app'],
+    domains: ['full-stack-ecommerce-clothing-web.vercel.app',"cdn.sanity.io"],
   },
 }
 

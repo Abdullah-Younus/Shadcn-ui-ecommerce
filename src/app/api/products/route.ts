8b32569b1c1d3b3c7a491fb2b39c9c7fc8e266ac
Abdullah-Younus <abdullah1001k@gmail.com>
2023-07-06 15:21:15 +0500

    local changes

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -4,7 +4,9 @@ import { client } from "@/lib/sanityClient";
 export async function GET(request: NextRequest) {
     try {
         let data = await client.fetch(`*[_type=='product']`)
-        return NextResponse.json({ data })
+        return NextResponse.json({
+            data: data
+        })
     } catch (error) {
         return NextResponse.json({ message: error })
     }

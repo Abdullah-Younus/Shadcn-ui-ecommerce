dbc0562d19b06bb59b5eb967b3169281c3635fac
Abdullah-Younus <abdullah1001k@gmail.com>
2023-07-06 17:13:59 +0500

    local changes

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { client } from "@/lib/sanityClient";
 
-export async function GET(request: NextRequest) {
+export async function GET(request: NextRequest, res: NextResponse) {
     try {
         let data = await client.fetch(`*[_type=='product']`)
         return NextResponse.json({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,20 @@ import ProductCarousel from '@/views/ProductCarousal/ProductCarousel';
 import { Image as IImage } from 'sanity';
 import { client } from '@/lib/sanityClient';
 
-
 async function getProductData() {
-
-  let res = await fetch(`${BASE_PATH_FORAPI}/api/products`)
-  return res.json();
-
+  try {
+    const res = await fetch(`${BASE_PATH_FORAPI}/api/products`);
+    if (!res.ok) {
+      throw new Error('Failed to fetch product data');
+    }
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    // Handle the error appropriately (e.g., show an error message to the user)
+  }
 }
+
 
 
 
@@ -23,7 +30,7 @@ async function Home() {
 
   let data = await getProductData();
 
-  console.log(data);
+  console.log('checking',data);
   return (
     <div>
       <Hero />

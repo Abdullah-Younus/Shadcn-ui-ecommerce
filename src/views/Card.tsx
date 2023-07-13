f64e8669fcc381ddab38f1a3e991527f8424cf5f
Abdullah-Younus <abdullah1001k@gmail.com>
2023-07-13 23:41:25 +0500

    local changes

--- src/views/Card.tsx
+++ src/views/Card.tsx
@@ -15,11 +15,10 @@ const Card: FC<{ singleProductData: oneProductType }> = ({ singleProductData }) 
         return builder.image(source);
     }
     return (
-        <div className="max-w-sm min-w-[24rem] space-y-3 select-none">
+        <div className="max-w-sm min-w-[24rem] space-y-3 select-none hover:scale-110 duration-300">
             <div className="relative w-full">
                 <div className="absolute inset-0 bg-gray-500 z-10">
                     <Image src={urlFor(singleProductData.image[0]).width(1000).height(1000).url()} alt="Logo" width={500} height={500} />
-
                 </div>
             </div>
             <div className="space-y-1 text-gray-600 font-semibold text-lg select-none">
--- src/views/ProductCarousal/ProductCarousel.tsx
+++ src/views/ProductCarousal/ProductCarousel.tsx
@@ -137,3 +137,66 @@ import Card from "../Card";
 // }
 
 
+
+const ProductCarousel: FC<{ ProductData: Array<oneProductType> }> = ({ ProductData }: any) => {
+    let initialX: number;
+    let isDragging = false;
+    let tabBox: any;
+
+    const isBrowser = () => typeof window !== "undefined";
+
+    if (isBrowser()) {
+        tabBox = document.querySelector(".scrollGrab");
+    }
+
+    // Desktop functions
+    function mouseMoves(e: any) {
+        if (!isDragging) return;
+        if (tabBox) {
+            tabBox.scrollLeft -= e.movementX;
+        }
+    };
+    function mouseDown() {
+        console.log("moving orignal")
+        isDragging = true;
+    }
+    function mouseUp() {
+        isDragging = false
+    }
+
+    // mobile functions
+    function mouseMovesForMobile(e: any) {
+        if (!isDragging) return;
+        if (tabBox) {
+            var currentX = e.touches[0].clientX;
+            var movementX = currentX - initialX;
+            tabBox.scrollLeft -= movementX / 5;
+        }
+    };
+    function mouseDownForMobile(e: any) {
+        isDragging = true;
+        initialX = e.touches[0].clientX;
+    };
+
+
+    return (
+        <div
+            onMouseMove={mouseMoves}
+            onMouseDown={mouseDown}
+            onMouseUp={mouseUp}
+            className="select-none flex gap-4 overflow-x-hidden scrollGrab"
+            onTouchMove={mouseMovesForMobile}
+            onTouchStart={mouseDownForMobile}
+            onTouchEnd={mouseUp}
+        >
+
+            {
+                ProductData.map((item: oneProductType, index: number) => (
+                    <Card key={index + 4} singleProductData={item} />
+                ))
+            }
+        </div>
+    )
+}
+
+export default ProductCarousel;

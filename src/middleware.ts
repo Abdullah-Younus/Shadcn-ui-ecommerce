ab27244473fa9a6650b7a60e11753895cb78c918
Abdullah-Younus <abdullah1001k@gmail.com>
2023-08-24 02:44:41 +0500

    local changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
     // "/" will be accessible to all users
-    publicRoutes: ["/", "/products", "/male", "/female", "/kids", "/api/cart:path*"]
+    publicRoutes: ["/", "/products", "/male", "/female", "/kids"],
 });
 
 export const config = {
-    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)", "/api/cart:path*"],
 };

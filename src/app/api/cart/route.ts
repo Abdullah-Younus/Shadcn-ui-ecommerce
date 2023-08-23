9964d30b5bc1e82aa2c5c89a6321975f62a9491a
Abdullah-Younus <abdullah1001k@gmail.com>
2023-08-24 02:39:16 +0500

    local changes
    local changes

--- src/app/api/cart/route.ts
+++ src/app/api/cart/route.ts
@@ -6,16 +6,15 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async (request: NextRequest, response: NextResponse) => {
 
-    const { user_id }: any = auth();
-    // const user_id = "12234adfsadfsafsa";
-
+    const { userId }: any = auth();
+    console.log('User_id',userId);
     const req: addToCart = await request.json();
 
     try {
 
-        if (req && user_id) {
+        if (req && userId) {
             await db.insert(cartTable).values({
-                user_id: user_id,
+                user_id: userId,
                 product_id: req.product_id,
                 product_name: req.product_name,
                 quantity: req.quantity,
@@ -39,16 +38,17 @@ export const POST = async (request: NextRequest, response: NextResponse) => {
 export const PUT = async (request: NextRequest, response: NextResponse) => {
 
     // const user_id = "12234adfsadfsafsa";
-    const { user_id }: any = auth();
+    const { userId }: any = auth();
+
 
 
     const data: addToCart = await request.json();
     try {
-        if (data && user_id) {
+        if (data && userId) {
             await db.update(cartTable).set({
                 quantity: data.quantity,
                 total_price: data.price,
-            }).where(and(eq(cartTable.user_id, user_id), eq(cartTable.product_id, data.product_id))).returning();
+            }).where(and(eq(cartTable.user_id, userId), eq(cartTable.product_id, data.product_id))).returning();
             return NextResponse.json({ message: "Data Updated SuccessFully" }, { status: 200 });
         } else {
             throw new Error("Error to Update Data");
@@ -64,13 +64,13 @@ export const PUT = async (request: NextRequest, response: NextResponse) => {
 export const DELETE = async (request: NextRequest) => {
 
     // const user_id = "12234adfsadfsafsa";
-    const { user_id }: any = auth();
+    const { userId }: any = auth();
 
     const Url = request.nextUrl;
     try {
-        if (Url.searchParams.has("product_id") && user_id) {
+        if (Url.searchParams.has("product_id") && userId) {
             const product_id = Url.searchParams.get("product_id");
-            const res = await db.delete(cartTable).where(and(eq(cartTable.user_id, user_id), eq(cartTable.product_id, product_id as any))).returning();
+            const res = await db.delete(cartTable).where(and(eq(cartTable.user_id, userId), eq(cartTable.product_id, product_id as any))).returning();
             return NextResponse.json({ message: "Cart Remove" }, { status: 200 });
 
         } else {
